55edb6f4019a7a82cd71f1ed4d0f5124486e7d5b
fassi842002 <47557474+fassi842002@users.noreply.github.com>
2025-01-31 23:19:53 +0100

    Update users.module.ts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,7 +2,7 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { PrismaModule } from 'src/prisma/prisma.module'; // Add this
+import { PrismaModule } from 'src/prisma/prisma.module'; 
 
 @Module({
   imports: [PrismaModule], // Import PrismaModule here
