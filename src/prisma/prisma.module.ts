3c1b76953dd48431f2f136fcf143b16077e3e512
fassi842002 <47557474+fassi842002@users.noreply.github.com>
2025-01-31 23:21:15 +0100

    Update prisma.module.ts

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,9 +1,9 @@
-// src/prisma/prisma.module.ts
+
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
 @Module({
   providers: [PrismaService],
-  exports: [PrismaService], // Export the service for dependency injection
+  exports: [PrismaService],
 })
 export class PrismaModule { }
